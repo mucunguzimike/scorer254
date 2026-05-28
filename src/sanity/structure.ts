99ec502ecc9b12dfd1560274122a2745f1fea3e7
mucunguzimike <163615298+mucunguzimike@users.noreply.github.com>
2026-05-28 13:14:49 +0300

    Fix hosted Sanity Studio configuration

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,17 @@
-import type {StructureResolver} from 'sanity/structure'
+import type {StructureResolver} from "sanity/structure"
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Scorer254 Studio")
+    .items([
+      S.documentTypeListItem("post").title("Articles"),
+      S.documentTypeListItem("author").title("Authors"),
+      S.documentTypeListItem("category").title("Categories"),
+      S.documentTypeListItem("tag").title("Tags"),
+      S.documentTypeListItem("team").title("Teams"),
+      S.documentTypeListItem("player").title("Players"),
+      S.documentTypeListItem("competition").title("Competitions"),
+      S.documentTypeListItem("region").title("Regions / Counties"),
+      S.divider(),
+      S.documentTypeListItem("siteSettings").title("Site Settings"),
+    ])
